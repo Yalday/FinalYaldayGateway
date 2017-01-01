435865848fd73f1d09bf8256f3266c72e5584050
Phil Quinn <philip.a.quinn@hotmail.co.uk>
2017-01-01 18:00:17 +0000

    initialized using jhipster

---
+++ src/main/java/com/yalday/gateway/config/liquibase/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Liquibase specific code.
+ */
+package com.yalday.gateway.config.liquibase;
---
+++ src/main/java/com/yalday/gateway/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.yalday.gateway.repository;
+
+import com.yalday.gateway.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
